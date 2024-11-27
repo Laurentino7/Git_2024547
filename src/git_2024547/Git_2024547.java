8280a4780d537b835cc7e351e0fbb7ae6d307b40
Laurentino7 <2024547@student.cct.ie>
2024-11-27 15:09:51 +0000

    Git test for homework

---
+++ src/git_2024547/Git_2024547.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package git_2024547;
+
+import java.util.Scanner;//// imports the Scanner class to allow the program to read user input.
+
+/**
+ *
+ * @author laure
+ */
+public class Git_2024547 {// defines the class named  git_2024547
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {// main method where program execution begins.
+        // Scanner to read user input
+        Scanner scanner = new Scanner(System.in);
+          System.out.println(" Laurentino Carlos - 2024547");// Display name  and student number
+    }
+    
+}
